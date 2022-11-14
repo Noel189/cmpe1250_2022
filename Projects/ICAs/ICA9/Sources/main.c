70d58f8c0ed4c2129f2ffeb7b331febe7c063329
Noel M Tesfe <ntesfe1@nait.ca>
2022-11-14 13:38:45 -0700

    files modified

--- Projects/ICAs/ICA9/Sources/main.c
+++ Projects/ICAs/ICA9/Sources/main.c
@@ -1,10 +1,10 @@
 /////////////////////////////////////////////////////////////////////////////
-// 9S12X Program: YourProg - MiniExplanation
+// 9S12X Program: ica9 - pit sleep characterization
 // Processor:     MC9S12XDP512
 // Bus Speed:     20 MHz (Requires Active PLL)
-// Author:        Simon Walker
+// Author:        Noel Tesfe
 // Details:       A more detailed explanation of the program is entered here
-// Date:          Date Created
+// Date:          11/14/2022
 // Revision History :
 //  each revision will have a date + desc. of changes
 
@@ -39,22 +39,25 @@ void main(void)
   // main entry point - these two lines must appear first
   _DISABLE_COP();
   EnableInterrupts;
-  
+
   /////////////////////////////////////////////////////////////////////////////
   // one-time initializations
   /////////////////////////////////////////////////////////////////////////////
-   PLL_To20MHz();
-   SWL_Init();
+  PLL_To20MHz();
+  SWL_Init();
   /////////////////////////////////////////////////////////////////////////////
   // main program loop
   /////////////////////////////////////////////////////////////////////////////
   for (;;)
   {
-     
-        PIT_Sleep(2000000ul,PITTF_Ch1,50);
-           SWL_TOG(SWL_RED); 
+    //  PART A
+    // PIT_Sleep(20000000ul,PITTF_Ch1,20);
+    //    SWL_TOG(SWL_RED);
 
-  }                   
+    // PART B
+    PIT_Sleep(20000000ul, PITTF_Ch1, 60);
+    SWL_TOG(SWL_RED);
+  }
 }
 
 /////////////////////////////////////////////////////////////////////////////
