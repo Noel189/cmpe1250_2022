7e36ceb6ed308274811fee31c179b3fb45c880c3
Noel <horizon33noela@gmail.com>
2022-12-15 08:16:59 -0700

    modified files

--- Projects/Labs/Lab04/Sources/main.c
+++ Projects/Labs/Lab04/Sources/main.c
@@ -27,25 +27,27 @@
 /////////////////////////////////////////////////////////////////////////////
 // Local Prototypes
 /////////////////////////////////////////////////////////////////////////////
-void  GenerateBits(char * pbuff2);
-void GenerateDecimal(char * pbuff3);
+void GenerateBits(char *pbuff2);
+void GenerateDecimal(char *pbuff3);
 /////////////////////////////////////////////////////////////////////////////
 // Global Variables
 /////////////////////////////////////////////////////////////////////////////
-float  value =00000.0;
-char buff[21]={0};
-unsigned char dispon=1;
-unsigned char curson=1;
-unsigned char blinkon=1;
-unsigned  int bitPos=0;
-char buff2[21]={0};
-unsigned char ix=2;
-unsigned char iy=3;
+float value = 00000.0;
+char buff[21] = {0};
+unsigned char dispon = 1;
+unsigned char curson = 1;
+unsigned char blinkon = 1;
+unsigned int bitPos = 0;
+char buff2[21] = {0};
 char buff3[21]={0};
-char buff4[21]={0};
-unsigned int oldState=0;
-unsigned int leftOldState=0;
-unsigned int ctrOldState=0;
+unsigned char ix = 2;
+unsigned char iy = 3;
+// char buff3[21] = {0};
+// char buff4[21] = {0};
+unsigned int oldState = 0;
+unsigned int leftOldState = 0;
+unsigned int ctrOldState = 0;
+
 /////////////////////////////////////////////////////////////////////////////
 // Constants
 /////////////////////////////////////////////////////////////////////////////
@@ -55,120 +57,122 @@ unsigned int ctrOldState=0;
 /////////////////////////////////////////////////////////////////////////////
 void main(void)
 {
-  // main entry point - these two lines must appear first
-  _DISABLE_COP();
+    // main entry point - these two lines must appear first
+    _DISABLE_COP();
 
-  EnableInterrupts;
+    EnableInterrupts;
 
-  /////////////////////////////////////////////////////////////////////////////
-  // one-time initializations
-  /////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////
+    // one-time initializations
+    /////////////////////////////////////////////////////////////////////////////
 
-  PLL_To20MHz();
-  SWL_Init();
-  Segs_Init();
-  Segs_Clear();
-  lcd_Init();
-  (void)PIT_Init(20000000ul,PITTF_Ch0,100000ul);
+    PLL_To20MHz();
+    SWL_Init();
+    Segs_Init();
+    Segs_Clear();
+    lcd_Init();
+    (void)PIT_Init(20000000ul, PITTF_Ch0, 100000ul);
 
-  /////////////////////////////////////////////////////////////////////////////
-  // main program loop
-  /////////////////////////////////////////////////////////////////////////////
-  for (;;)
-  {
+    GenerateBits(buff2);
+    lcd_StringXY(2, 3, buff2);
+    lcd_DispControl(dispon, curson, blinkon);
+
+    /////////////////////////////////////////////////////////////////////////////
+    // main program loop
+    /////////////////////////////////////////////////////////////////////////////
+    for (;;)
+    {
         // lcd_AddrXY(ix,iy);
-    lcd_DispControl(dispon,curson,blinkon);
-    // 
-    if(PITTF_PTF0)
-    {
-//    PIT_Sleep(20000000ul,PITTF_Ch1,100);
-  // acknowledge the flag
-      PITTF = PITTF_PTF0_MASK;//clears the flag
-    (void)sprintf(buff, "Count: %07.1f", value+=0.1);
-    lcd_StringXY(0, 0, buff);
-    }
 
-//display decimal DEC:
-    {
-        // GenerateDecimal(buff3);
-         (void)sprintf(buff3,"DEC: %d%d%d%d%d",0,0,0,0,0);
-         lcd_StringXY(0, 1, buff3);
-    }
-//display decimal HEX:
-    {
-        // GenerateDecimal(buff3);
-         (void)sprintf(buff4,"HEX: %X%X%X%X",0,0,0,0);
-         lcd_StringXY(0, 2, buff4);
-    }
-
-    {
-       
-     //centered at the bottom of the display show a 16bit binary number
-
-      GenerateBits(buff2);
-      lcd_StringXY(2, 3, buff2);
-      lcd_AddrXY(ix,iy);
-
-        
-    }
-
-    //when the right switch button is pressed
-    {
-        int curState = SWL_Pushed(SWL_RIGHT);
-        if((curState != oldState) && curState)
+        //
+        if (PITTF_PTF0)
         {
-
-            lcd_AddrXY(++ix,iy);
+            //    PIT_Sleep(20000000ul,PITTF_Ch1,100);
+            // acknowledge the flag
+            PITTF = PITTF_PTF0_MASK; // clears the flag
+            (void)sprintf(buff, "Count: %07.1f", value += 0.1);
+            lcd_StringXY(0, 0, buff);
         }
 
-        oldState=curState;
-    }
-
-    //when the left switch button is pressed
-
-    {
-        int curState = SWL_Pushed(SWL_LEFT);
-        if((curState != leftOldState) && curState)
+        // display decimal DEC:
         {
-            lcd_AddrXY(--ix,iy);
+            // GenerateDecimal(buff3);
+          
+            (void)sprintf(buff3, "DEC: %05d",0);
+            lcd_StringXY(0, 1, buff3);
         }
-        leftOldState= curState;
+        // display decimal HEX:
+        {
+            // GenerateDecimal(buff3);
+            (void)sprintf(buff3, "HEX: %04X", 0);
+            lcd_StringXY(0, 2, buff3);
+             lcd_AddrXY(ix, iy);
+        }
+
+        {
+
+            // centered at the bottom of the display show a 16bit binary number
+
+            //  lcd_Addrxy(2,3);
+
+           
+        }
+
+        // when the right switch button is pressed
+        {
+            int curState = SWL_Pushed(SWL_RIGHT);
+            if ((curState != oldState) && curState)
+            {
+
+                lcd_AddrXY(++ix, iy);
+            }
+
+            oldState = curState;
+        }
+
+        // when the left switch button is pressed
+
+        {
+            int curState = SWL_Pushed(SWL_LEFT);
+            if ((curState != leftOldState) && curState)
+            {
+                lcd_AddrXY(--ix, iy);
+            }
+            leftOldState = curState;
+        }
+
+        // when the center switch button is pressed
+        {
+            int curState = SWL_Pushed(SWL_CTR);
+            if ((curState != ctrOldState) && curState)
+            {
+                buff2[ix-2] = '1';
+                lcd_StringXY(2, 3, buff2);
+
+              
+               
+            }
+
+            ctrOldState = curState;
+        }
+
+        PIT_Sleep(20000000ul, PITTF_Ch1, 10);
     }
-
-    //when the center switch button is pressed
-    {
-        int curState= SWL_Pushed(SWL_CTR);
-       if((curState != ctrOldState) && curState){
-        buff2[ix]=1;
-       }
-
-       ctrOldState= curState;
-    }
-
-    PIT_Sleep(20000000ul, PITTF_Ch1, 10);
-
-  }
 }
 
 /////////////////////////////////////////////////////////////////////////////
 // Functions
 /////////////////////////////////////////////////////////////////////////////
-void  GenerateBits(char * pbuff2)
+void GenerateBits(char *pbuff2)
 {
- int i;
- for(i=0;i<16;i++){
-  pbuff2[i]='0';
-  pbuff2[16]=0;
- }
+    int i;
+    for (i = 0; i < 16; i++)
+    {
+        pbuff2[i] = '0';
+        pbuff2[16] = 0;
+    }
 }
 
-// void GenerateDecimal(char * pbuff3){
-// int i;
-//  for(i=0;i<5;i++){
-//   pbuff3[i]='0';
-//   pbuff3[5]=0;
-//  }
-// }
 /////////////////////////////////////////////////////////////////////////////
 // Interrupt Service Routines
 /////////////////////////////////////////////////////////////////////////////
